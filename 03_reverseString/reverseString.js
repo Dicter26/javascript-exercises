523b05d65a47d7f72091a42c29c5c701bb98cde8
Dicter26 <dicter.r26@ciencias.unam.mx>
2023-07-10 12:55:17 -0600

    Resolviendo los primeros ejercicios

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(strings) {
+    let reversedString = "";
+    for(let i = strings.length; i >= 0; i--){
+        reversedString += strings[i];
+    }
+    return reversedString.substring(9); //reversedString
 };
 
 // Do not edit below this line

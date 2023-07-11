23bb9eea1c34b2fcc4b9571148dc898bcbf2c912
Dicter26 <dicter.r26@ciencias.unam.mx>
2023-07-11 13:29:32 -0600

    Resolviendo los ejercicios basicos de javascript

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,32 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arrays, elementstodelete) {
+    let array = arguments[0];
+    let arraywithdeleted = [];
+    for(let i = 1; i < arguments.length; i++){
+        arraywithdeleted = deletefromArray(array, arguments[i]);
+    }
+    return cleanArray(arraywithdeleted);
 };
+
+function deletefromArray(array, param){
+    for(let i = 0; i < array.length; i++){
+        if(param === array[i]){
+            delete array[i];
+        }
+    }
+    return array;
+}
+
+function cleanArray(array){
+    let cleaned = []
+    for(let i = 0; i < array.length; i++){
+        if(array[i] !== undefined){
+            cleaned.push(array[i]);
+        }
+    }
+    console.log(cleaned);
+    return cleaned;
+}
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(begin, end) {
+    if(typeof(begin) !== "number" || typeof(end) !== "number"){
+        return "ERROR"
+    }
+    if(begin < 0 || end < 0){
+        return "ERROR"
+    }
+    if(begin > end){
+        return sum(end, begin);
+    }
+    return sum(begin, end);
 };
+
+function sum(begin, end){
+    let sum = 0;
+    for(let i = begin; i <= end; i++){
+        sum += i;
+    }
+    return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
